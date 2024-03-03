5f6edce31e964fc885c7edb9c725245b4d8e6282
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2024-03-04 00:10:08 +0200

    Update myPYcode.py

--- myPYcode.py
+++ myPYcode.py
@@ -1,5 +1,6 @@
 try:
-    from moviepy.editor import VideoFileClip, concatenate_videoclips, TextClip, CompositeVideoClip
+    # from moviepy.editor import VideoFileClip, concatenate_videoclips, TextClip, CompositeVideoClip
+    yuag.saveFile("error", "Errors.txt")
     # الكود الخاص بك هنا
 except Exception as error:
     yuag.saveFile(str(error), "Errors.txt")
