1e3933500a84fb3e6ca2e094a1b690d21640e543
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2024-03-04 00:32:46 +0200

    Update myPYcode.py

--- myPYcode.py
+++ myPYcode.py
@@ -3,6 +3,7 @@ import yuag
 
 try:
     subprocess.check_call(["pip", "install", "numpy"])
+    subprocess.check_call(["pip", "install", "proglog"])
     from moviepy.editor import VideoFileClip, concatenate_videoclips, TextClip, CompositeVideoClip
     yuag.saveFile("error", "Errors.txt")
     # الكود الخاص بك هنا
