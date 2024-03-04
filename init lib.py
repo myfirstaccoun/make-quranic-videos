7e76802d6f814a72f829216df8aceaff58b1efe6
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2024-03-04 11:48:47 +0200

    Create init lib.py

---
+++ init lib.py
@@ -0,0 +1,7 @@
+import subprocess
+
+subprocess.check_call(["pip", "install", "requests"])
+subprocess.check_call(["pip", "install", "decorator"])
+subprocess.check_call(["pip", "install", "imageio_ffmpeg"])
+subprocess.check_call(["pip", "install", "numpy"])
+subprocess.check_call(["pip", "install", "proglog"])
